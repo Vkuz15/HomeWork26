07af180957442b95b174080fea3337a1dd8a7267
Владимир <vkuznecov1504@mail.ru>
2024-06-03 20:52:53 +0400

    Написали метод grow который увеличивает массив в 1,5 раза, если место закончилось. Написали проверку на заполненность метода add. Заменили метод сортировки на рекурсивную.

--- src/main/java/org/example/IntegerListImpl.java
+++ src/main/java/org/example/IntegerListImpl.java
@@ -9,7 +9,7 @@ import java.util.Arrays;
 
 public class IntegerListImpl implements IntegerList {
 
-    private final Integer[] storage;
+    private Integer[] storage;
     private int size;
 
     public IntegerListImpl() {
@@ -19,9 +19,10 @@ public class IntegerListImpl implements IntegerList {
     public IntegerListImpl(int intSize) {
         storage = new Integer[intSize];
     }
+
     @Override
     public Integer add(Integer item) {
-        validateSize();
+        growIfNeeded();
         validateItem(item);
         storage[size++] = item;
         return item;
@@ -29,7 +30,7 @@ public class IntegerListImpl implements IntegerList {
 
     @Override
     public Integer add(int index, Integer item) {
-        validateSize();
+        growIfNeeded();
         validateItem(item);
         validateIndex(index);
         if (index == size) {
@@ -139,9 +140,9 @@ public class IntegerListImpl implements IntegerList {
         }
     }
 
-    private void validateSize() {
+    private void growIfNeeded() {
         if (size == storage.length) {
-            throw new StorageIsFullException();
+            grow();
         }
     }
 
@@ -152,15 +153,32 @@ public class IntegerListImpl implements IntegerList {
     }
 
     private void sort(Integer[] arr) {
-        for (int i = 1; i < arr.length; i++) {
-            int temp = arr[i];
-            int j = i;
-            while (j > 0 && arr[j - 1] >= temp) {
-                arr[j] = arr[j - 1];
-                j--;
-            }
-            arr[j] = temp;
+        quickSort(arr, 0, arr.length - 1);
+    }
+
+    public static void quickSort(Integer[] arr, int begin, int end) {
+        if (begin < end) {
+            int partitionIndex = partition(arr, begin, end);
+
+            quickSort(arr, begin, partitionIndex - 1);
+            quickSort(arr, partitionIndex + 1, end);
         }
+    }
+
+    private static int partition(Integer[] arr, int begin, int end) {
+        int pivot = arr[end];
+        int i = (begin - 1);
+
+        for (int j = begin; j < end; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+
+                swap(arr, i, j);
+            }
+        }
+
+        swap(arr, i + 1, end);
+        return i + 1;
     }
 
     private boolean binarySearch(Integer[] arr, Integer item) {
@@ -197,5 +215,15 @@ public class IntegerListImpl implements IntegerList {
 
     private void swapElements(Integer[] arr, int i, int minElementIndex) {
         sortSelection(arr);
+    }
+
+    private void grow() {
+        storage = Arrays.copyOf(storage, size + size / 2);
+    }
+
+    private static void swap(Integer[] list, int index1, int index2) {
+        int tmp = list[index1];
+        list[index1] = list[index2];
+        list[index2] = tmp;
     }
 }
